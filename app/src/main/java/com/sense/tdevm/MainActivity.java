352cf62409d0a584657210c969523f605df0a685
Tdevm <cr14b17@gmail.com>
2016-07-24 00:57:19 +0530

    Separated animateX & animateY

--- app/src/main/java/com/sense/tdevm/MainActivity.java
+++ app/src/main/java/com/sense/tdevm/MainActivity.java
@@ -28,39 +28,53 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         Sensor accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+
+    public void animateaccX(float x) {
+        ImageView img_animation = (ImageView) findViewById(R.id.layout_image);
+        if (x < -1) {
+            TranslateAnimation animation = new TranslateAnimation(0.0f, 233.4f, 0.0f, 0.0f);
+            animation.setDuration(1500);
+            //animation.setFillAfter(true);
+            img_animation.startAnimation(animation);
+        } else if (x > 5) {
+            TranslateAnimation animation = new TranslateAnimation(0.0f, -232.4f, 0.0f, 0.0f);
+            animation.setDuration(1500);
+            // animation.setFillAfter(true);
+            img_animation.startAnimation(animation);
+        }
+    }
+
+    public void animateaccY(float y) {
+        ImageView img_animation = (ImageView) findViewById(R.id.layout_image);
+        if (y > 5) {
+            TranslateAnimation animation = new TranslateAnimation(0.0f, 0.0f, 0.0f, 295.7f);
+            animation.setDuration(1500);
+            // animation.setFillAfter(true);
+            img_animation.startAnimation(animation);
+        } else if (y < -1) {
+            TranslateAnimation animation = new TranslateAnimation(0.0f, 0.0f, 0.0f, -388.5f);
+            animation.setDuration(1500);
+            //animation.setFillAfter(true);
+            img_animation.startAnimation(animation);
+        }
     }
     @Override
     public void onSensorChanged(final SensorEvent sensorEvent) {
         myButton = (Button)findViewById(R.id.start);
         myButton.setOnClickListener(new View.OnClickListener() {
-            float accX = sensorEvent.values[0];
-            float accY = sensorEvent.values[1];
-            float accZ = sensorEvent.values[2];
             @Override
             public void onClick(View view) {
-                ImageView img_animation = (ImageView) findViewById(R.id.layout_image);
-                if(accY>5) {
-                    TranslateAnimation animation = new TranslateAnimation(0.0f, 0.0f, 0.0f, 295.7f);
-                    animation.setDuration(1500);
-                   // animation.setFillAfter(true);
-                    img_animation.startAnimation(animation);
-                }else if(accX>5){
-                    TranslateAnimation animation = new TranslateAnimation(0.0f, -232.4f, 0.0f, 0.0f);
-                    animation.setDuration(1500);
-                   // animation.setFillAfter(true);
-                    img_animation.startAnimation(animation);
-                }else if(accY<-1){
-                    TranslateAnimation animation = new TranslateAnimation(0.0f, 0.0f, 0.0f, -388.5f);
-                    animation.setDuration(1500);
-                    //animation.setFillAfter(true);
-                    img_animation.startAnimation(animation);
-                }else if(accX<-1){
-                    TranslateAnimation animation = new TranslateAnimation(0.0f, 233.4f, 0.0f, 0.0f);
-                    animation.setDuration(1500);
-                    //animation.setFillAfter(true);
-                    img_animation.startAnimation(animation);
+                float accX = sensorEvent.values[0];
+                float accY = sensorEvent.values[1];
+                if (accX < -1 || accX > 5) {
+                    animateaccX(accX);
+                } else if (accY < -1 || accY > 5) {
+                    animateaccY(accY);
                 }
+
             }
         });
     }
